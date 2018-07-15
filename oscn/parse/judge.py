56744f69a372df1aeca88e2b1d1d688931c79417
jdungan <johnadungan@gmail.com>
2018-07-15 08:41:53 -0500

    add parser for judge

---
+++ oscn/parse/judge.py
@@ -0,0 +1,11 @@
+import re
+
+
+def judge(oscn_html):
+    judge_re = r'Judge:\s*([\w\s\,]*)'
+    find_judge = re.compile(judge_re, re.M)
+    judge_search = find_judge.search(oscn_html)
+    if judge_search.group:
+        return judge_search.group(1)
+    else:
+        return None
