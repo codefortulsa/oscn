2d41a0082a556a21fabf94eff30291a838182542
jdungan <johnadungan@gmail.com>
2018-08-03 16:16:54 -0500

    search package for py files

--- oscn/parse/__init__.py
+++ oscn/parse/__init__.py
@@ -1,34 +1,25 @@
+from os import listdir
+from os.path import splitext
 from importlib import import_module
 from types import FunctionType
 
-# these import statements expose parsers for use as oscn.parse.{function}
-from .counts import counts
-from .parties import parties
-from .judge import judge
-from .docket import docket
-from .events import events
-
-
-# The following code searches for parse function to allow them to be
+# The following code searches for parse functions to allow them to be
 # added to objects as properties using the append_parsers function
 
-parse_modules = ['counts',
-                 'dates',
-                 'docket',
-                 'events',
-                 'judge',
-                 'parties',
-                 ]
-
 parse_functions = []
-for py in parse_modules:
-    parse_module = import_module(f'.{py}', package='oscn.parse')
-    for name in dir(parse_module):
-        attr = getattr(parse_module, name)
-        if isinstance(attr, FunctionType):
-            if hasattr(attr,'target'):
-                parse_functions.append(attr)
 
+try:
+    for py_file in listdir(__path__[0]):
+        parse_module = import_module(f'.{splitext(py_file)[0]}', package=__package__)
+        for name in dir(parse_module):
+            attr = getattr(parse_module, name)
+            if isinstance(attr, FunctionType):
+                if hasattr(attr,'target'):
+                    # replaces 'from .counts import counts'
+                    locals()[name] = attr
+                    parse_functions.append(attr)
+except NameError:
+    pass
 
 def make_property(parse_function):
     return property(lambda self: parse_function(self.response.text))
