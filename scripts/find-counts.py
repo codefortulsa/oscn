a0d0e209a9878ab73ecd2a01808b08c1159b086b
jdungan <johnadungan@gmail.com>
2018-08-05 11:48:07 -0500

    bump version, update examples with new CaseList

--- scripts/find-counts.py
+++ scripts/find-counts.py
@@ -20,23 +20,18 @@ csv_file = open('data/find-counts.csv', "w")
 writer = csv.writer(csv_file, delimiter=',')
 writer.writerow(["year", "county", "case", "description"])
 
-for county in counties:
-    for year in years:
-        sys.stdout.write(f'{county} {year}')
-        case_iter = oscn.request.CaseList(county=county, year=year)
-        count = 0
-        for case in case_iter:
-            time.sleep(.10)
-            sys.stdout.write(case.case_number)
-            sys.stdout.flush()
+cases = oscn.request.CaseList(county=counties, year=years)
 
-            counts = oscn.parse.counts(case.response.text)
-            for count in counts:
-                if count_test(count['description']):
-                    writer.writerow([year, county, case.case_number, count['description']])
-                    sys.stdout.write('*')
-                else:
-                    sys.stdout.write('.')
-            sys.stdout.flush()
+for case in cases:
+    count = 0
+    sys.stdout.write(case.case_number)
+    sys.stdout.flush()
+    for count in case.counts:
+        if count_test(count['description']):
+            writer.writerow([case.year, case.county, case.case_number, count['description']])
+            sys.stdout.write('*')
+        else:
+            sys.stdout.write('.')
+    sys.stdout.flush()
 
 csv_file.close()
--- scripts/save-counts.py
+++ scripts/save-counts.py
@@ -7,24 +7,19 @@ import oscn
 counties = ['washington', 'cimarron', 'beckham']
 years = ['2017','2018']
 
+cases = oscn.request.CaseList(county=counties, year=years)
 
-for county in counties:
-    csv_file = open(f'data/{county}-counts.csv', "w")
-    # if this breaks, you may need to mkdir data
+for case in cases:
+    csv_file = open(f'data/counts.csv', "w")
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(["year", "county", "case", "judge", "description","source"])
-
-    for year in years:
-        sys.stdout.write(f'{county} {year}')
-        case_iter = oscn.request.CaseList(county=county, year=year)
-        for case in case_iter:
-            sys.stdout.write(case.case_number)
-            sys.stdout.flush()
-            for count in case.counts:
-                writer.writerow(
-                    [year, county, case.case_number, case.judge,
-                        count['description'], case.source])
-                sys.stdout.write('.')
-                sys.stdout.flush()
+    sys.stdout.write(case.case_number)
+    sys.stdout.flush()
+    for count in case.counts:
+        writer.writerow(
+            [case.year, case.county, case.case_number, case.judge,
+                count['description'], case.source])
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
     csv_file.close()
