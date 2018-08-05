2b0abe9f9c882639888408b3e5b5d9f0dcc17471
jdungan <johnadungan@gmail.com>
2018-08-05 11:52:37 -0500

    update example script with new CaseList

--- scripts/save-counts.py
+++ scripts/save-counts.py
@@ -12,7 +12,7 @@ cases = oscn.request.CaseList(county=counties, year=years)
 for case in cases:
     csv_file = open(f'data/counts.csv', "w")
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["year", "county", "case", "judge", "description","source"])
+    writer.writerow(["year", "county", "case", "judge", "description", "source"])
     sys.stdout.write(case.case_number)
     sys.stdout.flush()
     for count in case.counts:
