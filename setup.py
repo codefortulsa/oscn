f7cc658c872b1867d69e866341e4262d6254fd67
jdungan <johnadungan@gmail.com>
2018-08-10 20:35:46 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 
 setup(  name='oscn',
-        version='0.0.0.11',
+        version='0.0.0.12',
         description='Oklahoma Supreme Court Network page parsing utilities',
         long_description=long_description,
         long_description_content_type="text/markdown",
