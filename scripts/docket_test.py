fc5b78d897c57ad040632064d78fa9dbdb00cbfa
jdungan <johnadungan@gmail.com>
2018-11-11 12:47:04 -0600

    find reappear

--- scripts/docket_test.py
+++ scripts/docket_test.py
@@ -1,11 +1,17 @@
+import re
 import oscn
+# Create a test function
+search_for = re.compile(r'reappear', re.I | re.M)
+def test_minutes(docket):
+    for min in docket:
+        if search_for.search(min['description']):
+            return True
+    return False
 
-cases = oscn.request.CaseList(county='tulsa',year='2018', start=10, stop=20)
+# define the Case attr to test and the function to use
 
-for case in cases:
-    import ipdb; ipdb.set_trace()
-    print(case.case_number)
-    print(case.source)
-    for minute in case.docket:
-            print(minute)
-    print('----------------------------')
+cases = oscn.request.CaseList(county='tulsa', year='2018', start= 1, stop=350).find(docket=test_minutes)
+
+import ipdb; ipdb.set_trace()
+
+next(cases)
