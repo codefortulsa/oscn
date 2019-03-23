52efd0a024d42bc78b1a8561db976b7e3f687a55
jdungan <johnadungan@gmail.com>
2019-03-23 11:30:32 -0500

    trimming scripts

--- scripts/multicase_test.py
+++
@@ -1,9 +0,0 @@
-import oscn
-
-cases = oscn.request.CaseList(counties='delaware',years='2018', start=18, stop=22, types='CF')
-
-for case in cases:
-    print(case.number)
-    print(case.source)
-    print(f'judge: {case.judge} counts: {case.counts}')
-    print('----------------------------')
