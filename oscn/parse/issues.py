79c06003d67de189b9e43551d7a8050ff77ed63a
jdungan <johnadungan@gmail.com>
2021-09-29 17:28:59 -0500

    update issues for counties without disposition table

--- oscn/parse/issues.py
+++ oscn/parse/issues.py
@@ -35,31 +35,43 @@ def issues(oscn_html):
     soup = BeautifulSoup(oscn_html, "html.parser")
     start = soup.find("h2", "section issues")
     issue_table = start.find_next_sibling("table")
-    issue_list.text = issue_table.get_text(separator=" ")
 
-    while re.search("Issue #", issue_table.text):
-        # find the issue details
-        issue_dict = find_values(issue_table, issue_keys)
+    # if the next table is the docket there are no issue tables
+    is_docket = issue_table.find('thead')
+        
+    if is_docket:
+        next_sibling = start.find_next_sibling("p")
+        if next_sibling:
+            while next_sibling.name != "h2":
+                if next_sibling.name == "p":
+                    issue_list.append(clean_string(next_sibling.text))
+                next_sibling = next_sibling.next_sibling
 
-        # the next table should be dispositions
-        disp_table = issue_table.find_next_sibling("table")
-        issue_list.add_text(disp_table.get_text(separator=" "))
+    else:
+        issue_list.text = issue_table.get_text(separator=" ")
+        while re.search("Issue #", issue_table.text):
+            # find the issue details
+            issue_dict = find_values(issue_table, issue_keys)
 
-        issue_dict["parties"] = []
-        parties_rows = disp_table.find_all("tr")
-        for row in parties_rows:
-            # remove formatting elements from td
-            for td in row.find_all("td"):
-                td.string = " ".join(td.strings)
-            party_values = find_values(row, party_keys)
-            party_dict = make_party_dict(**party_values)
-            if party_dict:
-                issue_dict["parties"].append(party_dict)
+            # the next table should be dispositions
+            disp_table = issue_table.find_next_sibling("table")
+            issue_list.add_text(disp_table.get_text(separator=" "))
 
-        issue_list.append(issue_dict)
+            issue_dict["parties"] = []
+            parties_rows = disp_table.find_all("tr")
+            for row in parties_rows:
+                # remove formatting elements from td
+                for td in row.find_all("td"):
+                    td.string = " ".join(td.strings)
+                party_values = find_values(row, party_keys)
+                party_dict = make_party_dict(**party_values)
+                if party_dict:
+                    issue_dict["parties"].append(party_dict)
 
-        # get the next table
-        issue_table = disp_table.find_next_sibling("table")
+            issue_list.append(issue_dict)
+
+            # get the next table
+            issue_table = disp_table.find_next_sibling("table")
 
     return issue_list
 
---
+++ tests/test_issues.py
@@ -0,0 +1,9 @@
+import oscn
+
+
+def test_two_issue_formats():
+    case1 = oscn.request.Case("choctaw-SC-2020-118")
+    assert case1.issues[0] == "1. EVICTION SMALL CLAIMS(UNDER $5000.00)"
+
+    case2 = oscn.request.Case("tulsa-SC-2020-118")
+    assert case2.issues[0]['Issue'] == "FORCIBLE ENTRY & DETAINER <$5000.00. (SCFED1)"
