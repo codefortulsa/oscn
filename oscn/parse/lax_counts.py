4a71a4c89d3c2703f6e13287566c35fb8af2ae83
jdungan <johnadungan@gmail.com>
2024-10-27 12:38:21 -0500

    improve lax counts

--- oscn/parse/lax_counts.py
+++ oscn/parse/lax_counts.py
@@ -1,77 +1,67 @@
+import re
 from selectolax.parser import HTMLParser
 from ._helpers import clean_string, MetaList
-import re
+
+def extract_text_after_keyword(keyword, stop_keywords=None):
+    stop_pattern = "|".join(map(re.escape, stop_keywords)) if stop_keywords else "$"
+    pattern = re.compile(rf"{re.escape(keyword)}(.*?)(?:{stop_pattern})", re.DOTALL)
+
+    def extract(content):
+        match = pattern.search(content)
+        return clean_string(match.group(1)) if match else ""
+    
+    return extract
+
+# Curried extract functions with pre-compiled regex patterns
+extract_count_description = extract_text_after_keyword("Count as Filed:", ["in violation of"])
+extract_offense_date = extract_text_after_keyword("Date of Offense:", ["<br>"])
+extract_disposed_value = extract_text_after_keyword("Disposed:", ["Count as Disposed:"])
+
+def parse_count_container(counts_container):
+    count_info = {
+        "party": "",
+        "offense": "",
+        "description": "",
+        "disposed": "",
+        "violation": ""
+    }
+
+    # Extract Count Description and Date of Offense
+    count_description_td = counts_container.css_first("td.CountDescription")
+    if not count_description_td:
+        return None
+
+    count_description_text = clean_string(count_description_td.html)
+    count_info["description"] = extract_count_description(count_description_text)
+    count_info["offense"] = extract_offense_date(count_description_text)
+
+    # Extract Violation Link Text
+    violation_link = count_description_td.css_first("a[href]")
+    if violation_link:
+        count_info["violation"] = violation_link.text(separator=" ")
+
+    # Extract Party Name and Disposed Information
+    disposition_row = counts_container.css_first("table.Disposition tbody tr")
+    if disposition_row:
+        party_name_td = disposition_row.css_first("td.countpartyname nobr")
+        if party_name_td:
+            count_info["party"] = party_name_td.text().strip()
+
+        count_disposition_td = disposition_row.css_first("td.countdisposition")
+        if count_disposition_td:
+            count_disposition_text = count_disposition_td.text(separator=" ")
+            count_info["disposed"] = extract_disposed_value(count_disposition_text)
+
+    return count_info
 
 def counts(oscn_html):
     count_list = MetaList()
     tree = HTMLParser(oscn_html)
-    if (containers := tree.css("div.CountsContainer")):
-        # Extract counts information from "Counts" table
-        for counts_container in containers:
-            counts_container_text = clean_string(counts_container.html)
-            count_table = counts_container.css("table.Counts tbody tr")
-            count_description_td = count_table[0].css_first("td.CountDescription")
-            if not count_description_td:
-                continue
-            
-            # Extract text from CountDescription
-            count_description_text = clean_string(count_description_td.html)
-            # Extract description after "Count as Filed:" until "in violation of" or <br>
-            count_desc = ""
-            if "Count as Filed:" in count_description_text:
-                count_desc_parts = count_description_text.split("Count as Filed:")
-                if len(count_desc_parts) > 1:
-                    count_desc = count_desc_parts[1].split(", in violation of")[0]
-            # Use "Count as Disposed" if it exists to override description
-            if "Count as Disposed:" in counts_container_text:
-                count_as_disposed_parts = counts_container_text.split("Count as Disposed:")
-                if len(count_as_disposed_parts) > 1:
-                    count_desc = clean_string(count_as_disposed_parts[1].split('<br>')[0])
-
-            # Extract date of offense
-            offense_date = ""
-            if "Date of Offense:" in count_description_text:
-                offense_date_parts = count_description_text.split("Date of Offense:")
-                if len(offense_date_parts) > 1:
-                    offense_date = offense_date_parts[1].split('<br>')[0].strip()
-                    offense_date = clean_string(offense_date)
-
-            # Extract violation link text
-            violation_link = count_description_td.css_first("a[href]")
-            violated_statute = violation_link.text(separator=" ") if violation_link else ""
-
-            # Extract party name and disposed information from the "Disposition" table
-            disposition_row = counts_container.css_first("table.Disposition tbody tr")
-            if disposition_row:
-                party_name_td = disposition_row.css_first("td.countpartyname nobr")
-                party_name = party_name_td.text().strip() if party_name_td else ""
-
-                count_disposition_td = disposition_row.css_first("td.countdisposition")
-                if count_disposition_td:
-                    # Extract disposed information after "Disposed:" until <br> or end of line
-                    count_disposition_text = count_disposition_td.text(separator=" ")
-                    disposed_value = ""
-                    if "Disposed:" in count_disposition_text:
-                        disposed_parts = count_disposition_text.split("Count as Disposed:")
-                        if len(disposed_parts) > 1:
-                            disposed_value = clean_string(disposed_parts[0].split('Disposed:')[1])
-                else:
-                    disposed_value = ""
-            else:
-                party_name = ""
-                disposed_value = ""
-
-            save_count_info = {
-                "party": party_name,
-                "offense": offense_date,
-                "description": count_desc,
-                "disposed": disposed_value,
-                "violation": violated_statute,
-            }
-            count_list.append(save_count_info)
-
+    for counts_container in tree.css("div.CountsContainer"):
+        count_info = parse_count_container(counts_container)
+        if count_info:
+            count_list.append(count_info)
     return count_list
-
 
 setattr(counts, "target", ["Case"])
 setattr(counts, "_default_value", [])
